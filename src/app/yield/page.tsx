e990bb382721ebd1d92a938d05cb72a6aeeefb19
weilun <tangweilun0410@gmail.com>
2025-03-25 19:28:26 +0800

    removed unsed var

--- src/app/yield/page.tsx
+++ src/app/yield/page.tsx
@@ -9,14 +9,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
 import {
   AlertDialog,
-  AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { HistoryIcon, Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
