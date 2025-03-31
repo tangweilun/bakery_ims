f9bfac5e25d9cad87f4ba589aec4efdf9bff972f
weilun <tangweilun0410@gmail.com>
2025-03-31 20:39:05 +0800

    Add external file to tellNetlify not to bundle @tensorflow/tfjs-node with function code. Instead, it will be included as an external dependency.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // Only apply this on the server-side build
+    if (isServer) {
+      // Add @tensorflow/tfjs-node to externals to prevent it from being bundled
+      config.externals = [...(config.externals || []), "@tensorflow/tfjs-node"];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
